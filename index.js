c9415573a637946f89f0bf04e348dc58460fad88
Brian Roster <broster@newforma.com>
2015-12-12 09:43:20 -0500

    Couple of missing files

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-console.log('Loading function');
-
 exports.handler = function (event, context) {
-
-    var async = require("async");
-/*     var sdk = require('aws-sdk'); 
-    var s3 = new sdk.S3(); 
-
-    var zlib = require('zlib');
-    var gzip = zlib.createGzip();
-    
-    var params = {
-        Bucket: 'brianroster',
-        Key: 'Sunset.jpg'
-    };
-
-    var req = s3.getObject(params, function(err, data) {
-        if (err) {
-            console.log('error');
-            console.log(err, err.stack); // an error occurred
-        }
-        else {
-            console.log('ok');
-            console.log(data.ContentLength);           // successful response
-        }
-    }); */
-
-    context.succeed('ok');
+    console.log('OK ' + event.name);
+    context.done(null, 'complete');
 };
