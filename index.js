8d8d77208253ad389560c7fcfb861ade8fd7608e
Brian Roster <broster@newforma.com>
2015-12-12 10:37:41 -0500

    Fully deployable and locally testable lambda

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var aws = require('aws-sdk')
+var async = require('async')
+
 exports.handler = function (event, context) {
     console.log('OK ' + event.name);
     context.done(null, 'complete');
